f4d6cf301efd90e302c5279a28aa65d781e5cc67
Jesús Aguilar <146678995+jesus1aguilop@users.noreply.github.com>
2024-11-17 14:00:39 -0500

    crudpython

---
+++ static/js/scrollEffect.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const fadeElements = document.querySelectorAll('.fade-in');
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    });
+
+    fadeElements.forEach(element => {
+        observer.observe(element);
+    });
+});
